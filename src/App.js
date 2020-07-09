f4a4dc9352658df1bb50d1a84d9480e44633f878
MargaritaMak <margarita.pogosyan.96@mail.ru>
2020-07-10 01:06:31 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Items from './components/Items';
 import ModalWindow from './components/ModalWindow';
@@ -7,73 +7,61 @@ import './App.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      cart: [],
-      isModalOpen: false,
-    }
-  }
+const App = () => {
+  const [items, setItems] = useState([])
+  const [cart, setCart] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-  componentDidMount() {
+
+  useEffect (() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
         return response.json();
       })
       .then(data => {
-        this.setState({
-          items: data,
-        })
+        setItems(data)
       })
-  }
+  }, []);
 
-  onClick = (id) => {
-    const {items, cart} = this.state;
-    const cartItem = cart.find(item =>{
+  const onClick = (id) => {
+    const cartItem = cart.find(item => {
       return item.id === id
     })
-    if(cartItem){
-      return 
+    if (cartItem) {
+      return
     }
-    const item = items.find(item =>{
+    const item = items.find(item => {
       return item.id === id
     })
     if (item) {
-      this.setState({
-        cart: [item, ...this.state.cart]
-      })
-    }
-}
-
-onItemDelete = (id) => {
-  this.setState({
-    cart: this.state.cart.filter(item => item.id !== id)
-  })
-}
-onCartClick = () => {
-  this.setState({
-    isModalOpen: !this.state.isModalOpen
-  })
-}
-
-    render() {
-      const {items} = this.state;
-      return (
-        <div className="App">
-          <Navbar onCartClick = {this.onCartClick} itemCount = {this.state.cart.length}/>
-          {this.state.isModalOpen ? 
-          <ModalWindow values={this.state.cart} onItemDelete = {this.onItemDelete} /> : null
-          }
-          <main>
-            {items.map(item => (
-              <Items key={item.id} id={item.id} title={item.title} description={item.body} onAdd = {this.onClick}/>
-            ))}
-          </main>
-          </div>
-      );
+      setCart([item, ...cart])
     }
   }
+
+  const handleItemDelete = (id) => {
+    setCart(cart.filter(item => item.id !== id))
+  }
+
+  const handleCartClick = () => {
+    setIsModalOpen(!isModalOpen)
+  }
+
+
+
+  return (
+    <div className="App">
+      <Navbar onCartClick={handleCartClick} itemCount={cart.length} />
+      {isModalOpen ?
+        <ModalWindow values={cart} onItemDelete={handleItemDelete} /> : null
+      }
+      <main>
+        {items.map(item => (
+          <Items key={item.id} id={item.id} title={item.title} description={item.body} onAdd={onClick} />
+        ))}
+      </main>
+    </div>
+  );
+}
+
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = (props) => {
-return <button onClick = {props.onClick}>{props.value}</button>
+return <button {...props}>{props.value}</button>
 }
 
 
